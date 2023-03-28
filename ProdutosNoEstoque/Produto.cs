aafec103425269c698e06217d77e458c99811e49
gabb-z <gabbz.liima@gmail.com>
2023-03-27 21:14:15 -0300

    v1 - Projeto ProdutosNoEstoque

---
+++ ProdutosNoEstoque/Produto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProdutosNoEstoque
+{
+    class Produto
+    {
+        //Atributos - Produto
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //Construtor
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        //Função - Valor Total
+        public double ValorTotal()
+        {
+            return Preco * Quantidade;
+        }
+
+        //Função - Adicionar Produto
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        //Função - Remover Produto
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        //ToString
+        public override string ToString()
+        {
+            return Nome + ", R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidade(s), Total R$: " + ValorTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ProdutosNoEstoque/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProdutosNoEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entrada de Dados - Produto
+            Console.WriteLine("Digite os dados do produto: ");
+            Console.Write("Nome: ");
+            string nomeProduto = Console.ReadLine();
+            Console.Write("Preço: ");
+            double precoProduto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int qtdeEstoque = int.Parse(Console.ReadLine());
+
+            //Instância de Objeto
+            Produto prod = new Produto(nomeProduto, precoProduto, qtdeEstoque);
+
+            //Saída de Dados - Produto
+            Console.WriteLine($"\nDados do produto: {prod}");
+
+            //Entrada de Dados - Add. Produtos
+            Console.Write("\nDigite o número de produto(s) a serem adicionado(s) ao estoque: ");
+            int qtde = int.Parse(Console.ReadLine());
+            prod.AdicionarProduto(qtde);
+
+            //Saída de Dados Atualizada - Produto
+            Console.WriteLine($"\nDados atualizados do produto: {prod}");
+
+            //Entrada de Dados - Remover Produtos
+            Console.Write("\nDigite o número de produto(s) a serem adicionado(s) ao estoque: ");
+            qtde = int.Parse(Console.ReadLine());
+            prod.RemoverProduto(qtde);
+
+            //Saída de Dados Atualizada - Produto
+            Console.WriteLine($"\nDados atualizados do produto: {prod}");
+        }
+    }
+}
